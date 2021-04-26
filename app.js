cb6e65c89f0fbe6f864689b9427b1de39477063e
WilliamWu <995347291@qq.com>
2021-04-26 18:02:59 +0800

    基础中间件

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 const config = require('./config')
 
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
-app.get('/', (req, res) => {
+// 引入中间件
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.post('/', (req, res) => {
   res.send('OK')
 })
 
