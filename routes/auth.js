4607638d5eb0ad7ba0aac1cf3c91a7ddbc7187f4
WilliamWu <995347291@qq.com>
2021-04-30 11:17:57 +0800

    登录接口

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,10 @@ const router = require('express').Router()
 
 const auth = require('../controller/auth')
 
+const validator = require('../middleware/validate')
+const { userValidator } = require('../model/user')
+
 // 登录
-router.post('/', auth.test)
+router.post('/', validator(userValidator), auth.test)
 
 module.exports = router
