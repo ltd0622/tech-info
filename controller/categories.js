a29c412f0431f6d80246ebdd578751f9206563d9
WilliamWu <995347291@qq.com>
2021-05-06 10:37:42 +0800

    添加分类接口

--- controller/categories.js
+++ controller/categories.js
@@ -1,3 +1,6 @@
+// 引入分类 Model
+const { Category } = require('../model/categories')
+
 // 获取全部
 exports.getAll = (req, res, next) => {
   try {
@@ -8,9 +11,29 @@ exports.getAll = (req, res, next) => {
 }
 
 // 添加新的
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   try {
-    res.send('添加新的')
+    // 1 检测是否已存在要添加的分类信息
+    const data = req.body
+    let cate = await Category.findOne(data)
+    // 分类信息已存在
+    if (cate) {
+      return res.status(400).json({
+        code: 400,
+        msg: '该分类已存在',
+        value: data
+      })
+    }
+
+    // 2 创建数据并保存
+    cate = new Category(data)
+    await cate.save()
+
+    res.status(200).json({
+      code: 200,
+      msg: '分类添加成功',
+      data
+    })
   } catch (err) {
     next(err)
   }
@@ -34,6 +57,7 @@ exports.update = (req, res, next) => {
   }
 }
 
+// 删除
 exports.remove = (req, res, next) => {
   try {
     res.send('删除某个')
