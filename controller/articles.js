45b52133aaa130dd9d46bdd5ff1d7e4ce3906f94
WilliamWu <995347291@qq.com>
2021-05-07 09:45:44 +0800

    添加文章接口

--- controller/articles.js
+++ controller/articles.js
@@ -1,3 +1,6 @@
+// 引入文章 Model
+const { Article } = require('../model/articles')
+
 // 获取全部
 exports.getAll = (req, res, next) => {
   try {
@@ -8,9 +11,18 @@ exports.getAll = (req, res, next) => {
 }
 
 // 添加新的
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   try {
-    res.send('添加新的')
+    // 1 创建并存储数据
+    const data = new Article(Object.assign(req.body, { author: req.userData._id }))
+    await data.save()
+
+    // 2 响应
+    res.status(200).json({
+      code: 200,
+      msg: '添加文章成功',
+      data
+    })
   } catch (err) {
     next(err)
   }
