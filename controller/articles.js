f19282d272ea75815e5e06a7b0994e779aa09de2
WilliamWu <995347291@qq.com>
2021-05-07 18:03:35 +0800

    获取所有文章接口

--- controller/articles.js
+++ controller/articles.js
@@ -2,9 +2,21 @@
 const { Article } = require('../model/articles')
 
 // 获取全部
-exports.getAll = (req, res, next) => {
+exports.getAll = async (req, res, next) => {
   try {
-    res.send('获取全部')
+    // 检测是否存在分类/状态等筛选参数
+    const { status, category } = req.query
+    let data
+    if (status || category) {
+      data = await Article.find(req.query)
+    } else {
+      data = await Article.find()
+    }
+    res.status(200).json({
+      code: 200,
+      msg: '获取所有文章成功',
+      data
+    })
   } catch (err) {
     next(err)
   }
