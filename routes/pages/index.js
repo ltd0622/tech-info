89852e82453b34a4f93bf95f521e54ff85c3d13f
WilliamWu <995347291@qq.com>
2021-05-11 17:49:41 +0800

    分类数据处理

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,8 +1,19 @@
 const Router = require('express').Router()
 
+// 引入 Model
+const { Category } = require('../../model/categories')
+
 // 首页路由与分类路由
-Router.get(['/', '/:cid'], (req, res) => {
-  res.render('index.html')
+Router.get(['/', '/:cid'], async (req, res) => {
+  // 1 读取数据库，获取分类数据
+  const cate = await Category.find()
+
+
+  // 将数据传递给模板引擎
+  res.render('index.html', {
+    cate,
+    current: req.params.cid
+  })
 })
 
 // 文章页路由
